8aa10ac2ad1dc8efacac8a815a4ae23773a6b73e
ValentinRouquet <mrtuara19@gmail.com>
2025-05-05 13:32:16 +0200

    Ajout script d'automatisation

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Ou voulez-vous enregistrer le projet ? (Chemin absolu ex : c:/dossier)"
+read directory
+
+echo "Quel est le nom de votre projet ?"
+read project
+
+cd "$directory" || exit
+mkdir "$project"
+cd "$project"
+touch index.html style.css README.md
+
+echo "Le projet a été ajouté avec succès."
+
